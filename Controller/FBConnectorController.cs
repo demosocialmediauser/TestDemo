2182ca65914be8b84819ea10188146fd1f29baae
demosocialmediauser <117079603+demosocialmediauser@users.noreply.github.com>
2022-11-02 15:56:05 +0530

    FBConnector - first Checkin

---
+++ Controller/FBConnectorController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace FBConnector.Controller
+{
+    public class FBConnectorController : ApiController
+    {
+        public string Get()
+        {
+            return "FB Connector Running";
+        }
+    }
+}
