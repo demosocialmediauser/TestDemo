eb3b9f41e34b07ff053100b3149f702e81ef09a8
demosocialmediauser <117079603+demosocialmediauser@users.noreply.github.com>
2022-11-22 13:19:05 +0530

    Update JsonModel.cs

--- Model/JsonModel.cs
+++ Model/JsonModel.cs
@@ -6,26 +6,39 @@ namespace FBConnector.Model
 {
    
        
-       public class From
+        public class Change
     {
-        public string name { get; set; }
+        public Value value { get; set; }
+        public string field { get; set; }
+    }
+
+    public class Entry
+    {
         public string id { get; set; }
+        public int time { get; set; }
+        public List<Change> changes { get; set; }
+    }
+
+    public class From
+    {
+        public string id { get; set; }
+        public string name { get; set; }
     }
 
     public class JsonDataModel
     {
-        public string field { get; set; }
-        public Value value { get; set; }
+        public List<Entry> entry { get; set; }
+        public string @object { get; set; }
     }
 
     public class Value
     {
-        public string item { get; set; }
-        public string post_id { get; set; }
-        public string verb { get; set; }
-        public int published { get; set; }
-        public int created_time { get; set; }
-        public string message { get; set; }
         public From from { get; set; }
+        public string post_id { get; set; }
+        public int created_time { get; set; }
+        public string item { get; set; }
+        public string parent_id { get; set; }
+        public string reaction_type { get; set; }
+        public string verb { get; set; }
     }
     }
