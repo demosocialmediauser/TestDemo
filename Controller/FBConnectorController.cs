19d1f971e67d37124192497003fed46ee12e369d
demosocialmediauser <117079603+demosocialmediauser@users.noreply.github.com>
2022-11-22 14:03:28 +0530

    Update FBConnectorController.cs

--- Controller/FBConnectorController.cs
+++ Controller/FBConnectorController.cs
@@ -1,32 +1,24 @@
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.Runtime.InteropServices.WindowsRuntime;
 using System.Web.Http;
 
 namespace FBConnector.Controller
 {
     public class FBConnectorController : ApiController
     {
-        public string Get()
+        public dynamic Get()
         {
-            string returnValue = "No file created yet";
-            try
-            {
-                
-                string textFile = AppDomain.CurrentDomain.BaseDirectory + "Response.txt";
-                if (File.Exists(textFile))
-                {
-                    returnValue = File.ReadAllText(textFile);
-
-                }
-            }
-            catch(Exception ex)
-            {
-                returnValue = ex.ToString();
+            dynamic returnValue = null;
+            string textFile = AppDomain.CurrentDomain.BaseDirectory + "Response.txt";
+            if (File.Exists(textFile))
+            {                   
+                returnValue = JsonConvert.DeserializeObject<dynamic>(File.ReadAllText(textFile));                
+               
             }
             return returnValue;
 
